ad0f9b998163944f04205b7342e97ccdc4706d19
zero86 <katanazero86@gmail.com>
2020-03-31 18:32:00 +0900

    예제 작성

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import '../scss/App.scss';
 
-export default function App() {
+// 함수형 컴포넌트
+// props 매개변수를 가진다 (컴포넌트 속성)
+export default function App(props) {
+
+    const { name, age } = { ...props };
 
     return (
         <div className="hello-wrap">
             <p className="hello-wrap-content">Hello React World!!!!</p>
+            <p>{`${name}은 ${age}살`}</p>
         </div>
     );
     
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App'
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App name='zero86' age='20'/>, document.getElementById('app'));
